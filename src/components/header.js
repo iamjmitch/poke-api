1500cafd4239bced6fa901754226c4b2d4d0ce25
James Mitchell <iamjmitch1990@gmail.com>
2021-05-20 00:44:59 +1000

    mobile css tweak

--- src/components/header.js
+++ src/components/header.js
@@ -21,7 +21,8 @@ const StyledHeader = styled.div`
   @media (max-width: 500px) {
     flex-direction: row;
     background: #ad3529;
-    padding-bottom: 15px;
+    padding: 0 10px 15px 10px;
+
     img {
       height: unset;
       width: 30%;
